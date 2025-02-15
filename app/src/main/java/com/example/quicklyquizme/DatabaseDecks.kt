e35c76cdad6d88542aaa32dc12178903f9cbef00
anemafa <jeremyponce.deguzman@gmail.com>
2025-02-16 03:23:28 +0800

    added card functions and some deck functions

--- app/src/main/java/com/example/quicklyquizme/DatabaseDecks.kt
+++ app/src/main/java/com/example/quicklyquizme/DatabaseDecks.kt
@@ -19,7 +19,9 @@ class DatabaseDecks (private val context: Context): SQLiteOpenHelper(context,DAT
         newVersion: Int
     ) {
         val dropTableQuery="DROP TABLE IF EXISTS $TABLE_NAME"
+        val dropTableQuery2="DROP TABLE IF EXISTS $CARD_TABLE"
         db?.execSQL(dropTableQuery)
+        db?.execSQL(dropTableQuery2)
         onCreate(db)
     }
     fun insertDeck(deckName :String){
@@ -31,19 +33,85 @@ class DatabaseDecks (private val context: Context): SQLiteOpenHelper(context,DAT
         db.close()
         return
     }
+    fun insertCard(cardFront:String, cardBack: String,deck_id:Long){
+        val values= ContentValues().apply{
+            put(CARD_FRONT, cardFront)
+            put(CARD_BACK,cardBack)
+            put(CARD_FK,deck_id)
+        }
+        val db= writableDatabase
+        db.insert(CARD_TABLE, null, values)
+        db.close()
+        return
+    }
     fun returnDeckAmount():Long{
         val db=readableDatabase
         val rv=DatabaseUtils.queryNumEntries(db, TABLE_NAME)
         db.close()
         return rv
     }
-    fun returnDeckName(deckID:Long): String {
+    fun returnDeckCardsAmount(deck_id: Int):Long{
+        val db=readableDatabase
+        val rv=DatabaseUtils.queryNumEntries(db, TABLE_NAME,"deck_id=$deck_id")
+        db.close()
+        return rv
+    }
+    fun returnFrontCards(deck_id: Long): Array<String> {
+        val db =readableDatabase
+        val cursor =db.query(CARD_TABLE,arrayOf("$CARD_FRONT"),"$CARD_FK=?",arrayOf(deck_id.toString()),null,null,null)
+        var rv=emptyArray<String>()
+        for (currentCard in 0..returnDeckCardsAmount(deck_id.toInt())){
+            cursor.moveToPosition(currentCard.toInt())
+            rv +=cursor.getString(0)
+        }
+        cursor.close()
+        return rv
+    }
+    fun editCard(card_id:Long, cardFront: String,cardBack: String){
+        val db =writableDatabase
+        val values=ContentValues().apply {
+            put(CARD_FRONT, cardFront)
+            put(CARD_BACK, cardBack)
+        }
+        db.update(CARD_TABLE,values,"$CARD_ID=?",arrayOf(card_id.toString()))
+    }
+    fun returnBackCards(deck_id: Long): Array<String> {
+        val db =readableDatabase
+        val selection="$CARD_FK=?"
+        val columns=arrayOf("$CARD_BACK")
+        val cursor=db.query(CARD_TABLE,
+            columns,selection,arrayOf(deck_id.toString()),null,null,null)
+        var rv=emptyArray<String>()
+        for (currentCard in 0..returnDeckCardsAmount(deck_id.toInt())){
+            cursor.moveToPosition(currentCard.toInt())
+            rv +=cursor.getString(0)
+        }
+        cursor.close()
+        db.close()
+        return rv
+    }
+    fun returnCardIDs(deck_id:Long): Array<String>{
+        val db =readableDatabase
+        val selection="$CARD_FK=?"
+        val columns=arrayOf(CARD_ID)
+        val cursor=db.query(CARD_TABLE,
+            columns,selection,arrayOf(deck_id.toString()),null,null,null)
+        var rv=emptyArray<String>()
+        for (currentCard in 0..returnDeckCardsAmount(deck_id.toInt())){
+            cursor.moveToPosition(currentCard.toInt())
+            rv +=cursor.getString(0)
+        }
+        cursor.close()
+        db.close()
+        return rv
+    }
+    fun returnDeckName(deckID:Long):String {
         val db=readableDatabase
         val selection = "$COLUMN_ID=?"
         val selectionArgs= arrayOf( deckID.toString())
         var rv =""
         val cursor= db.query(TABLE_NAME,
-            arrayOf("ID"), selection, selectionArgs, null, null,null)
+            arrayOf("$COLUMN_DECK_NAME"), selection, selectionArgs, null, null,null)
         if (cursor.moveToFirst()){
             rv =cursor.getString(0)
         }
@@ -53,23 +121,35 @@ class DatabaseDecks (private val context: Context): SQLiteOpenHelper(context,DAT
     }
     fun deleteDeck(deckID:Long){
         val db=writableDatabase
-        db.delete(TABLE_NAME,"deckID=?", arrayOf(deckID.toString()))
+        db.delete(TABLE_NAME,"$COLUMN_ID=?", arrayOf(deckID.toString()))
         for (IDnumber in 0..returnDeckAmount()){
             if (IDnumber>deckID){
                 val newID=IDnumber-1
                 val values = ContentValues()
-                values.put("deckID",newID)
-                db.update(TABLE_NAME, values,"deckID=?",arrayOf((IDnumber).toString()))
+                values.put(COLUMN_ID,newID)
+                db.update(TABLE_NAME, values,"$COLUMN_ID?",arrayOf((IDnumber).toString()))
             }
         }
         db.close()
-        return
+    }
+    fun deleteCard(card_id:Long){
+        val db =writableDatabase
+        db.delete(CARD_TABLE,"$CARD_ID=?",arrayOf(card_id.toString()))
+        for (IDnumber in 0..returnDeckAmount()){
+            if (IDnumber>card_id){
+                val newID=IDnumber-1
+                val values = ContentValues()
+                values.put(CARD_ID,newID)
+                db.update(CARD_TABLE, values,"$CARD_ID=?",arrayOf((IDnumber).toString()))
+            }
+        }
+        db.close()
     }
     fun renameDeck(deckID: Long,newName:String){
         val db=writableDatabase
         val values=ContentValues()
-        values.put("deckName",newName)
-        db.update(TABLE_NAME,values,"deckID=?",arrayOf(deckID.toString()))
+        values.put(COLUMN_DECK_NAME,newName)
+        db.update(TABLE_NAME,values,"$COLUMN_ID=?",arrayOf(deckID.toString()))
         db.close()
         return
     }
@@ -84,6 +164,5 @@ class DatabaseDecks (private val context: Context): SQLiteOpenHelper(context,DAT
         private const val CARD_FRONT="front"
         private const val CARD_BACK="back"
         private const val CARD_FK="deckID"
-
     }
 }
