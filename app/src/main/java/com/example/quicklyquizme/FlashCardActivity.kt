80041225b9415dc9e92526ca5eca32baa9fff63d
anemafa <jeremyponce.deguzman@gmail.com>
2025-02-17 04:51:33 +0800

    starting off

--- app/src/main/java/com/example/quicklyquizme/FlashCardActivity.kt
+++ app/src/main/java/com/example/quicklyquizme/FlashCardActivity.kt
@@ -1,4 +1,29 @@
 package com.example.quicklyquizme
 
-class FlashCardActivity {
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.example.quicklyquizme.databinding.ActivityFlashCardBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class FlashCardActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFlashCardBinding
+    private lateinit var deckDatabase: DeckDatabase
+    private lateinit var dataList: ArrayList<dataClass>
+    private lateinit var addDeck: FloatingActionButton
+    private lateinit var deckList: MutableList<String>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFlashCardBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
 }
