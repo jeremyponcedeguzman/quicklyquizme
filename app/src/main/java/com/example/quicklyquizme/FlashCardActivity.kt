293eb3e051733044908a83ae14e00a6d76ef088f
anemafa <jeremyponce.deguzman@gmail.com>
2025-02-18 16:54:58 +0800

    Update FlashCardActivity.kt
    edited on mobile
    probably all the functions necessary

--- app/src/main/java/com/example/quicklyquizme/FlashCardActivity.kt
+++ app/src/main/java/com/example/quicklyquizme/FlashCardActivity.kt
@@ -9,7 +9,10 @@ import com.example.quicklyquizme.databinding.ActivityFlashcardBinding
 
 class FlashCardActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFlashcardBinding
-//    private lateinit var deckDatabase:DeckDatabase
+				private lateinit var nextBtn: Button
+				private lateinit var flipBtn: Button
+				private lateinit var backBtn: Button
+				private lateinit var flashCardText:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFlashcardBinding.inflate(layoutInflater)
@@ -19,8 +22,31 @@ class FlashCardActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-//        deckDatabase=DeckDatabase(this)
-
+        }val frontCards:Array<String>=intent.getExtras?("frontCards")
+val backCards:Array<String>=intent.getExtras?("backCards")
+var frontFacing=true
+var currentCard=0
+val cardsAmount=.intent.getExtras?("cardsAmount")
+flashCardText=binding.flashCard
+flashCardText.text=frontCards.getString(currentCard)
+nextBtn=binding.nextBtn
+flipBtn=binding.flipBtn
+flipBtn.setOnClickListener{
+if (frontFacing){
+frontFacing=false
+flashCardText.text=backCards.getString(currentCard)
+}else{
+flashCardText.text=backCards.getString(currentCard)
+frontFacing=true}
+}
+nextBtn.setOnClickListener{
+currentCard++
+if (currentCard==cardsAmount){
+finish()
+}
+flashCardText.text=frontCards.getString(currentCard)}
+backBtn=binding.backBtn
+backBtn.setOnClickListener{
+finish()}
     }
 }
