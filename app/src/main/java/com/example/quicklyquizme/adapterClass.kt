43954e437fe724c95f5c8f25d8eace7f4ac6892c
anemafa <jeremyponce.deguzman@gmail.com>
2025-02-18 08:20:29 +0800

    added pop up menu functionality, ability to manipulate database within, and the button for the pop up menu

--- app/src/main/java/com/example/quicklyquizme/adapterClass.kt
+++ app/src/main/java/com/example/quicklyquizme/adapterClass.kt
@@ -4,7 +4,10 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.PopupMenu
 import android.widget.TextView
+import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 
@@ -14,7 +17,7 @@ class adapterClass(private val dataList: ArrayList<dataClass>): RecyclerView.Ada
     {
         val rvTitle: TextView=itemView.findViewById(R.id.deckName)
         val rvCard:CardView=itemView.findViewById(R.id.deckItem)
-
+        val rvImageButton:ImageButton=itemView.findViewById(R.id.imageButton)
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderClass {
         val itemView= LayoutInflater.from(parent.context).inflate(R.layout.item_layout,parent,false)
@@ -22,16 +25,48 @@ class adapterClass(private val dataList: ArrayList<dataClass>): RecyclerView.Ada
     }
     override fun onBindViewHolder(holder: ViewHolderClass, position: Int) {
         val currentItem=dataList[position]
-        holder.rvTitle.text=currentItem.deckName
+        val deckDatabase=currentItem.deckDatabase
+        holder.rvTitle.text=deckDatabase.returnDeckName(currentItem.currentID)
         holder.rvTitle.isSelected=true
         holder.rvCard.setOnClickListener{
             val intent=Intent(it.context,FlashCardActivity::class.java)
             it.context.startActivity(intent)
         }
+        holder.rvImageButton.setOnClickListener{
+            val popup=PopupMenu(it.context,holder.rvImageButton)
+            popup.inflate(R.menu.popup_menu)
+            popup.setOnMenuItemClickListener{ menuItem ->
+                when(menuItem.itemId){
+                    R.id.choiceMode->{
+                        Toast.makeText(it.context,"Choice",Toast.LENGTH_LONG).show()
+                        true
+                    }
+                    R.id.viewDeck->{
+                        val intent=Intent(it.context,DeckViewerActivity::class.java)
+                        intent.putExtra("deckID",currentItem.currentID)
+                        Toast.makeText(it.context,"view",Toast.LENGTH_LONG).show()
+                        it.context.startActivity(intent)
+                        true
+                    }
+                    R.id.deleteDeck->{
+                        deckDatabase.deleteDeck(currentItem.currentID)
+                        Toast.makeText(it.context,"Deck Deleted",Toast.LENGTH_LONG).show()
+                        val intent=Intent(it.context,MainActivity::class.java)
+                        it.context.startActivity(intent)
+                        true
+                    }
+                    else -> {false}
+                }
+            }
+            popup.show()
+        }
     }
     override fun getItemCount(): Int {
         return dataList.size
     }
+
 }
+
+
 
 
