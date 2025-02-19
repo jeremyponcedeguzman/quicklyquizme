7e7c22eeb7f8d121a7505c969db39d4e6714008b
anemafa <jeremyponce.deguzman@gmail.com>
2025-02-20 06:07:35 +0800

    MainActivity context now passed to it, code modified to accomodate that passes 2  pre-shuffled arrays to FlashCardActivity for it to properly function

--- app/src/main/java/com/example/quicklyquizme/adapterClass.kt
+++ app/src/main/java/com/example/quicklyquizme/adapterClass.kt
@@ -1,14 +1,16 @@
 package com.example.quicklyquizme
 
+import android.app.Activity
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
+import android.widget.Button
 import android.widget.PopupMenu
 import android.widget.TextView
 import android.widget.Toast
 import androidx.cardview.widget.CardView
+import androidx.core.app.ActivityCompat.recreate
 import androidx.recyclerview.widget.RecyclerView
 
 class adapterClass(private val dataList: ArrayList<dataClass>): RecyclerView.Adapter<adapterClass.ViewHolderClass>()
@@ -17,7 +19,7 @@ class adapterClass(private val dataList: ArrayList<dataClass>): RecyclerView.Ada
     {
         val rvTitle: TextView=itemView.findViewById(R.id.deckName)
         val rvCard:CardView=itemView.findViewById(R.id.deckItem)
-        val rvImageButton:ImageButton=itemView.findViewById(R.id.imageButton)
+        val rvButton: Button =itemView.findViewById(R.id.deckOptions)
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderClass {
         val itemView= LayoutInflater.from(parent.context).inflate(R.layout.item_layout,parent,false)
@@ -25,15 +27,26 @@ class adapterClass(private val dataList: ArrayList<dataClass>): RecyclerView.Ada
     }
     override fun onBindViewHolder(holder: ViewHolderClass, position: Int) {
         val currentItem=dataList[position]
-        val deckDatabase=currentItem.deckDatabase
+        val deckDatabase=DeckDatabase(currentItem.mainActivityContext)
         holder.rvTitle.text=deckDatabase.returnDeckName(currentItem.currentID)
         holder.rvTitle.isSelected=true
         holder.rvCard.setOnClickListener{
             val intent=Intent(it.context,FlashCardActivity::class.java)
+            val frontCardsList= mutableListOf<String>()
+            val backCardsList= mutableListOf<String>()
+            val cardList=deckDatabase.returnCardIDs(currentItem.currentID)
+            cardList.shuffle()
+            for (card in cardList){
+                frontCardsList.add(deckDatabase.returnFrontCard(card))
+                backCardsList.add(deckDatabase.returnBackCard(card))
+            }
+            intent.putExtra("frontCards",frontCardsList as ArrayList)
+            intent.putExtra("backCards",backCardsList as ArrayList)
             it.context.startActivity(intent)
+
         }
-        holder.rvImageButton.setOnClickListener{
-            val popup=PopupMenu(it.context,holder.rvImageButton)
+        holder.rvButton.setOnClickListener{
+            val popup=PopupMenu(it.context,holder.rvButton)
             popup.inflate(R.menu.popup_menu)
             popup.setOnMenuItemClickListener{ menuItem ->
                 when(menuItem.itemId){
@@ -44,15 +57,13 @@ class adapterClass(private val dataList: ArrayList<dataClass>): RecyclerView.Ada
                     R.id.viewDeck->{
                         val intent=Intent(it.context,DeckViewerActivity::class.java)
                         intent.putExtra("deckID",currentItem.currentID)
-                        Toast.makeText(it.context,"view",Toast.LENGTH_LONG).show()
                         it.context.startActivity(intent)
                         true
                     }
                     R.id.deleteDeck->{
                         deckDatabase.deleteDeck(currentItem.currentID)
                         Toast.makeText(it.context,"Deck Deleted",Toast.LENGTH_LONG).show()
-                        val intent=Intent(it.context,MainActivity::class.java)
-                        it.context.startActivity(intent)
+                        recreate(currentItem.mainActivityContext as Activity)
                         true
                     }
                     else -> {false}
@@ -64,7 +75,6 @@ class adapterClass(private val dataList: ArrayList<dataClass>): RecyclerView.Ada
     override fun getItemCount(): Int {
         return dataList.size
     }
-
 }
 
 
