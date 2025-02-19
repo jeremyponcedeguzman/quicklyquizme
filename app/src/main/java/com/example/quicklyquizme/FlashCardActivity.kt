b8f51ca6404639d094e5e1199c7fe5196609a6ab
anemafa <jeremyponce.deguzman@gmail.com>
2025-02-20 06:11:27 +0800

    no idea committing in case it's necessary for anything

--- app/src/main/java/com/example/quicklyquizme/FlashCardActivity.kt
+++ app/src/main/java/com/example/quicklyquizme/FlashCardActivity.kt
@@ -1,15 +1,24 @@
 package com.example.quicklyquizme
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 import com.example.quicklyquizme.databinding.ActivityFlashcardBinding
 
 class FlashCardActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFlashcardBinding
-//    private lateinit var deckDatabase:DeckDatabase
+    private lateinit var correctBtn: Button
+    private lateinit var wrongBtn: Button
+    private lateinit var flipBtn: Button
+    private lateinit var backBtn: Button
+    private lateinit var flashCardText:TextView
+    private lateinit var deckTitle: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFlashcardBinding.inflate(layoutInflater)
@@ -20,7 +29,58 @@ class FlashCardActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-//        deckDatabase=DeckDatabase(this)
+        val frontCards= intent.extras!!.getStringArrayList("frontCards")
+        val backCards=intent.extras!!.getStringArrayList("backCards")
+        var currentCard=0
+        val cardsAmount= frontCards?.size
+        var correctAnswers=0
+        val intent= Intent(this,SessionEndActivity::class.java)
+        deckTitle=binding.title
+        deckTitle.text=intent.extras?.getString("deckName")
+        flashCardText=binding.flashcard
+        flashCardText.text= frontCards!!.get(currentCard)
+        correctBtn=binding.correctBtn
+        wrongBtn=binding.wrongBtn
+        flipBtn=binding.flipBtn
+        flipBtn.setOnClickListener{
+            if (flashCardText.text!= backCards!![currentCard]){
+                flashCardText.text= backCards[currentCard]
+            }else{
+                flashCardText.text= frontCards[currentCard]
+                }
+            correctBtn.isVisible=true
+            wrongBtn.isVisible=true
+        }
+        correctBtn.setOnClickListener{
+            currentCard++
+            correctAnswers++
+            if (currentCard== (cardsAmount)!!){
+                intent.putExtra("score", "$correctAnswers/$cardsAmount")
+                startActivity(intent)
+                finish()
+            }
+            else{
+                flashCardText.text= frontCards[currentCard]
+                correctBtn.isVisible=false
+                wrongBtn.isVisible=false
+            }
+        }
+        wrongBtn.setOnClickListener{
+            currentCard++
+            if (currentCard== (cardsAmount)!!){
+                intent.putExtra("score", "$correctAnswers/$cardsAmount")
+                startActivity(intent)
+                finish()
+            }
+            else{
+                flashCardText.text= frontCards[currentCard]
+                correctBtn.isVisible=false
+                wrongBtn.isVisible=false
+            }
+        }
 
+        backBtn=binding.returnBtn
+        backBtn.setOnClickListener{
+            finish()}
     }
 }
